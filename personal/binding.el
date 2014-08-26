9dabee40daee29f0c7c05d650840d072ca6a027a
Carlo Bertini <waydotnet@gmail.com>
2014-08-26 09:17:35 +0200

    my personal porting

---
+++ personal/binding.el
@@ -0,0 +1,162 @@
+;;; personal-bindings.el -- Personal keybinds
+;;; Commentary:
+;;; List of personal shortcuts
+;;; Code:
+
+;; Navigate between windows using Alt-1, Alt-2, Shift-left, shift-up, shift-right
+(windmove-default-keybindings)
+
+;; Enable copy and pasting from clipboard
+(setq x-select-enable-clipboard t)
+
+(defvar my-keys-minor-mode-map (make-keymap) "WaY-Key")
+
+(define-minor-mode my-keys-minor-mode
+  "A minor mod for my custom keys"
+  t " my-keys" 'my-keys-minor-mode-map)
+(my-keys-minor-mode t)
+
+;; Non ci sta bisogno di commentare questo codice
+;; (define-key my-keys-minor-mode-map (kbd "RET") 'newline-and-indent)
+
+
+;; open current directory in iTerm2 from Emacs
+;; http://dlvr.it/5ySmRy
+(define-key my-keys-minor-mode-map (kbd "C-0") 'iterm-here)
+
+;; move current line up or down
+(define-key my-keys-minor-mode-map [(meta shift up)] 'move-line-up)
+(define-key my-keys-minor-mode-map [(meta shift down)] 'move-line-down)
+
+ ;; fullscreen
+(define-key my-keys-minor-mode-map (kbd "M-RET") 'toggle-fullscreen)
+
+ ;; find-temp-file
+(define-key my-keys-minor-mode-map (kbd "C-x C-t") 'find-temp-file)
+
+;; project-explorer
+(define-key my-keys-minor-mode-map (kbd "M-1") 'project-explorer-open)
+
+;; cambio il mapping di default di taglio (C-w)
+(define-key my-keys-minor-mode-map [remap kill-region] 'cut-line-or-region)
+
+;; cambio il mapping di default di copia (M-w)
+(define-key my-keys-minor-mode-map [remap kill-ring-save] 'copy-line-or-region)
+
+(define-key my-keys-minor-mode-map (kbd "M-x") 'smex)
+(define-key my-keys-minor-mode-map (kbd "M-X") 'smex-major-mode-commands)
+;; This is your old M-x.
+(define-key my-keys-minor-mode-map (kbd "C-c C-c M-x") 'execute-extended-command)
+
+
+ (add-hook 'focus-out-hook (lambda () (save-some-buffers t)))
+
+;; use regex searches by default.
+(define-key my-keys-minor-mode-map (kbd "C-s") 'isearch-forward-regexp)
+(define-key my-keys-minor-mode-map (kbd "\C-r") 'isearch-backward-regexp)
+(define-key my-keys-minor-mode-map (kbd "C-M-s") 'isearch-forward)
+(define-key my-keys-minor-mode-map (kbd "C-M-r") 'isearch-backward)
+
+(define-key my-keys-minor-mode-map (kbd "C-x O") (lambda ()
+                                (interactive)
+                                (other-window -1)))
+(define-key my-keys-minor-mode-map (kbd "C-x C-o") (lambda ()
+                                  (interactive)
+                                  (other-window 2)))
+
+;; Create ad empty buffer
+(define-key my-keys-minor-mode-map (kbd "C-c C-n") (generate-new-buffer "temp"))
+
+;; Start a regular shell if you prefer that.
+(define-key my-keys-minor-mode-map (kbd "C-x M-m") 'shell)
+
+;; Fetch the contents at a URL, display it raw.
+(define-key my-keys-minor-mode-map (kbd "C-x C-h") 'view-url)
+
+;; auto-complete-mode
+(define-key my-keys-minor-mode-map (kbd "M-q") 'auto-complete-m)
+
+;; comment line
+(define-key my-keys-minor-mode-map (kbd "M-7") 'comment-or-uncomment-current-line-or-region)
+
+;; OCCUR
+(define-key my-keys-minor-mode-map (kbd "C-c C-o") 'occur)
+;; redo+
+(define-key my-keys-minor-mode-map (kbd "C-?") 'redo)
+
+;; helm imenu
+(define-key my-keys-minor-mode-map (kbd "M-2") 'helm-semantic-or-imenu)
+
+;; whitespace
+(define-key my-keys-minor-mode-map (kbd "C-*") 'whitespace-mode)
+
+;; django
+(define-key my-keys-minor-mode-map (kbd "C-x j") 'python-django-open-project)
+
+;; openlaszlo
+(define-key my-keys-minor-mode-map (kbd "C-c C-c C-c") 'rslazlo-compile-file)
+
+;; hide ^M in svn-output
+(define-key my-keys-minor-mode-map (kbd "C-c C-.") 'remove-dos-eol)
+
+;; Align your code in a pretty way.
+(define-key my-keys-minor-mode-map (kbd "C-x \\") 'align-regexp)
+
+;; Font size
+(define-key my-keys-minor-mode-map (kbd "C-+") 'text-scale-increase)
+(define-key my-keys-minor-mode-map (kbd "C--") 'text-scale-decrease)
+
+;; Window switching. (C-x o goes to the next window)
+(define-key my-keys-minor-mode-map (kbd "C-x O") (lambda ()
+                                (interactive)
+                                (other-window -1))) ;; back one
+;; kill lines backward
+(define-key my-keys-minor-mode-map (kbd "C-<backspace>") (lambda ()
+                                        (interactive)
+                                        (kill-line 0)
+                                        (indent-according-to-mode)))
+
+;; kill-other-buffers
+(define-key my-keys-minor-mode-map (kbd "C-c C-c C-k") 'kill-other-buffers)
+
+;; Activate occur easily inside isearch
+(define-key isearch-mode-map (kbd "C-c o")
+  (lambda () (interactive)
+    (let ((case-fold-search isearch-case-fold-search))
+      (occur (if isearch-regexp
+                 isearch-string
+               (regexp-quote isearch-string))))))
+
+;; use hippie-expand instead of dabbrev
+(define-key my-keys-minor-mode-map (kbd "M-/") 'hippie-expand)
+
+;; replace buffer-menu with ibuffer
+(define-key my-keys-minor-mode-map (kbd "C-x C-b") 'ibuffer)
+
+;; magiìt
+(define-key my-keys-minor-mode-map (kbd "C-x g") 'magit-status)
+(define-key my-keys-minor-mode-map (kbd "C-x p") 'magit-push)
+
+;; expand region!
+(define-key my-keys-minor-mode-map (kbd "C-o") 'er/expand-region)
+
+;; jump
+(define-key my-keys-minor-mode-map (kbd "C-c SPC") 'ace-jump-mode)
+;; (define-key my-keys-minor-mode-map (kbd "C-c j") 'ace-jump-mode)
+(define-key my-keys-minor-mode-map (kbd "s-.") 'ace-jump-mode)
+(define-key my-keys-minor-mode-map (kbd "C-c J") 'ace-jump-buffer)
+(define-key my-keys-minor-mode-map (kbd "s->") 'ace-jump-buffer)
+
+;; muoviamoci tra le window
+(define-key my-keys-minor-mode-map (kbd "M-<left>")  'windmove-left)
+(define-key my-keys-minor-mode-map (kbd "M-<right>") 'windmove-right)
+(define-key my-keys-minor-mode-map (kbd "M-<up>")    'windmove-up)
+(define-key my-keys-minor-mode-map (kbd "M-<down>")  'windmove-down)
+
+
+(add-hook 'js2-mode-hook
+          #'(lambda ()
+              (define-key my-keys-minor-mode-map (kbd "C-c i") 'js-doc-insert-function-doc)
+              (define-key my-keys-minor-mode-map (kbd "@") 'js-doc-insert-tag)))
+
+;;; personal-bindings.el ends here
---
+++ personal/waydotnet.el
@@ -0,0 +1,232 @@
+;;; pacakge -- waydotnet.el
+;;; commentary:
+;; bypassiamo la schermata di benvenuto
+
+;; requirement
+;; (prelude-require-package 'parenface)
+(prelude-require-package 'nyan-mode)
+(prelude-require-package 'highlight-indentation)
+(prelude-require-package 'smart-mode-line)
+(prelude-require-package 'find-temp-file)
+(prelude-require-package 'diff-hl)
+(prelude-require-package 'ido-vertical-mode)
+(prelude-require-package 'workgroups2)
+(prelude-require-package 'project-explorer)
+(prelude-require-package 'rainbow-delimiters)
+;;(prelude-require-package 'ox-latex)
+(prelude-require-package 'visual-regexp)
+(prelude-require-package 'indent-guide)
+(prelude-require-package 'anaconda-mode)
+(prelude-require-package 'company-anaconda)
+(prelude-require-package 'pyenv-mode)
+
+
+;;; Code:
+(setq inhibit-startup-message t)
+;; impostiamo il tab di default a 2 caratteri
+(setq-default tab-width 2)
+;; usiamo gli spazi al posto dei tab !!!
+(setq-default indent-tabs-mode nil)
+;; show column number in mode-line
+(setq column-number-mode t)
+
+;; store all backup and autosave files in the tmp dir
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+
+;; fun :D
+(require 'nyan-mode)
+(nyan-mode)
+
+;; flat !
+(set-face-attribute 'header-line nil :box nil)
+(set-face-attribute 'mode-line nil :box nil)
+(set-face-attribute 'mode-line-inactive nil :box nil)
+
+(require 'workgroups2)
+;; autoload/autosave:
+;; if you start Emacs as "emacs --daemon" - turn off autoloading of workgroups:
+(setq wg-use-default-session-file t)
+
+
+;; save/restore opened files and windows config
+(desktop-save-mode +1) ; 0 for off
+(desktop-read)
+;; salviamo la posizione del cursore quando chiudo un buffer
+;; in modo da ripristinarla
+(require 'saveplace)
+(setq save-place-file (concat user-emacs-directory "saveplace"))
+
+;; change color of bracket, curly
+;; (require 'parenface)
+;; (eval-after-load 'parenface
+;;   (progn
+;;     (set-face-foreground 'parenface-paren-face "dark gray")
+;;     (set-face-foreground 'parenface-bracket-face "gray")
+;;     (set-face-foreground 'parenface-curly-face "gray")))
+
+;; (add-hook 'lisp-mode-hook 'paren-face-add-keyword)
+;; ;; (add-hook 'js2-mode-hook 'paren-face-add-keyword)
+;; (add-hook 'python-mode-hook 'paren-face-add-keyword)
+;; (add-hook 'emacs-lisp-mode-hook 'paren-face-add-keyword)
+;; (add-hook 'emacs-program-mode-hook 'paren-face-add-keyword)
+
+;; ASPELL !!!
+;; $ sudo port -v selfupdate
+;; $ sudo port install aspell-dict-en
+(setq ispell-program-name "/opt/local/bin/aspell")
+
+;; smart-mode-line
+(setq sml/theme 'dark)
+(sml/setup)
+
+;; python mode
+(add-hook 'python-mode-hook 'anaconda-mode)
+(add-to-list 'company-backends 'company-anaconda)
+(pyenv-mode)
+
+;; full screen toggle using command+[RET]
+(defun toggle-fullscreen ()
+  (interactive)
+  (set-frame-parameter nil 'fullscreen (if (frame-parameter nil 'fullscreen)
+                                           nil
+                                         'fullboth)))
+
+;; How to copy from one dired dir to the next dired dir shown in a split window?
+(setq dired-dwim-target t)
+
+
+(set-frame-font "Source Code Pro-11")
+
+;; Highlight Current Line
+(global-hl-line-mode +1)
+(require 'highlight-indentation)
+
+(setq frame-char (frame-char-width (selected-frame)))
+;; disabilito la acapo automatico
+(global-visual-line-mode t)
+
+;; use fuzzy style matching for commands aswell
+(require 'smex)
+(smex-initialize)
+(require 'find-temp-file)
+
+;; Show the current function name in the header line
+(which-function-mode)
+(setq which-func-unknown "n/a")
+(setq-default header-line-format
+              '((which-func-mode ("" which-func-format " "))))
+(setq mode-line-misc-info
+      ;; We remove Which Function Mode from the mode line, because it's mostly
+      ;; invisible here anyway.
+      (assq-delete-all 'which-func-mode mode-line-misc-info))
+
+;; line numbering with linum (WTF it's not activated by defaults?!)
+(global-linum-mode 1)
+(setq linum-disabled-modes-list '(mu4e-headers-mode))
+(defun linum-on ()
+  (unless (or (minibufferp) (member major-mode linum-disabled-modes-list))
+    (linum-mode 1)))
+;; disable linum for speedbar
+(add-hook 'speedbar-mode-hook  '(lambda () (linum-mode 0)))
+
+(require 'diff-hl)
+(global-diff-hl-mode 1)
+;; ido mode per il completamento dei file e dir
+(ido-mode 1)
+(ido-everywhere 1)
+;; flx mode .. like sublimeText cmd-T
+(flx-ido-mode 1)
+;; disable ido faces to see flx highlights.
+(setq ido-use-faces nil)
+;; preferisco il vertical !
+(ido-vertical-mode t)
+
+(require 'workgroups2)
+;; autoload/autosave:
+;; if you start Emacs as "emacs --daemon" - turn off autoloading of workgroups:
+(setq wg-use-default-session-file t)
+
+;; utf-8 powaa!!
+(set-language-environment 'utf-8)
+(prefer-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(setq locale-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(set-input-method nil)
+
+
+
+;; uniquify buffer names
+(require 'uniquify) ; bundled with GNU emacs 23.2.1 or before. On in 24.4
+(setq uniquify-buffer-name-style 'post-forward-angle-brackets) ; emacs 24.4 style ⁖ cat.png<dirName>
+(setq uniquify-buffer-name-style 'reverse)
+(setq uniquify-separator "/")
+(setq uniquify-after-kill-buffer-p t) ; rename after killing uniquified
+(setq uniquify-ignore-buffers-re "^\\*") ; don't muck with special buffers
+
+(require 'rainbow-delimiters)
+(global-rainbow-delimiters-mode)
+
+;; visual-regexp
+(require 'visual-regexp)
+(define-key global-map (kbd "C-c r") 'vr/replace)
+(define-key global-map (kbd "C-c q") 'vr/query-replace)
+
+;; EDITOR
+(global-auto-revert-mode 1)
+
+;; linea di identazione
+(require 'indent-guide)
+(setq-default indent-guide-mode t)
+(indent-guide-global-mode)
+
+;; change color of bracket, curly
+;; (require 'parenface)
+;; (eval-after-load 'parenface
+;;   (progn
+;;     (set-face-foreground 'parenface-paren-face "dark gray")
+;;     (set-face-foreground 'parenface-bracket-face "gray")
+;;     (set-face-foreground 'parenface-curly-face "gray")))
+
+;; org-mode
+;; HACK: http://nickhigham.wordpress.com/2013/07/05/emacs-org-mode-version-8/
+;;
+;; fix org-mode 8:
+;; start emacs without custom (emacs -Q)
+;; delete org (rm -rf ~/.emacs.d/elpa/org-TAB)
+;; M-x package-install org
+
+;;;;;;; ;; Include the latex-exporter
+;;;;;;; (require 'ox-latex)
+;;;;;;; ;; Add minted to the defaults packages to include when exporting.
+;;;;;;; (add-to-list 'org-latex-packages-alist '("" "minted"))
+;;;;;;; ;; Tell the latex export to use the minted package for source
+;;;;;;; ;; code coloration.
+;;;;;;; (setq org-latex-listings 't)
+;;;;;;; (setq org-latex-listings 'minted)
+
+;;;;;;; ;; #+LATEX_HEADER: \input{ABSOLUTEPATH\lib.tex}
+
+;;;;;;; ;; Let the exporter use the -shell-escape option to let latex
+;;;;;;; ;; execute external programs.
+;;;;;;; ;; This obviously and can be dangerous to activate!
+;;;;;;; (setq org-latex-pdf-process
+;;;;;;;       '("xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
+;;;;;;; ;; org-mode syntax color for code block :D
+;; (setq org-src-fontify-natively t)
+
+;; EX zencode... C-j per attivare la preview del autocompletamento
+;;(require 'emmet-mode)
+(add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+(add-hook 'web-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+(add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
+;; (add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2))) ;; indent 2 spaces.
+
+
+(provide 'waydotnet)
+;;; waydotnet.el ends here
